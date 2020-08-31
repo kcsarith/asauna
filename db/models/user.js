ba71bf4f86905968a875cda3243167dc922b4ef7
Krisna Sarith <krisnasarith@gmail.com>
2020-08-31 10:13:09 -0700

    Check users migration

--- db/models/user.js
+++ db/models/user.js
@@ -28,6 +28,27 @@ module.exports = (sequelize, DataTypes) => {
           len: [60, 60],
         },
       },
+      imageUrl: {
+        type: Sequelize.STRING,
+      },
+      displayBackgroundUrl: {
+        type: DataTypes.STRING
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      pronouns: {
+        type: DataTypes.STRING
+      },
+      roles: {
+        type: DataTypes.STRING
+      },
+      departmentOrTeam: {
+        type: DataTypes.STRING
+      },
+      aboutMe: {
+        type: DataTypes.TEXT
+      },
     },
     {
       defaultScope: {
@@ -46,19 +67,26 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
 
-  User.associate = function(models) {
+  User.associate = function (models) {
   };
 
-  User.prototype.toSafeObject = function() {
+  User.prototype.toSafeObject = function () {
     const {
       id,
-      username
+      username,
+      imageUrl,
+      displayBackgroundUrl,
+      pronouns,
+      status,
+      roles,
+      departmentOrTeam,
+      aboutMe
     } = this;
 
-    return { id, username };
+    return { id, username, imageUrl, displayBackgroundUrl, pronouns, status, roles, departmentOrTeam, aboutMe };
   };
 
-  User.login = async function({ username, password }) {
+  User.login = async function ({ username, password }) {
     const user = await User.scope('loginUser').findOne({
       where: {
         [Op.or]: [{ username }, { email: username }],
@@ -69,15 +97,15 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
 
-  User.prototype.validatePassword = function(password) {
+  User.prototype.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.hashedPassword.toString());
   };
 
-  User.getCurrentUserById = async function(id) {
+  User.getCurrentUserById = async function (id) {
     return await User.scope("currentUser").findByPk(id);
   };
 
-  User.signup = async function({ username, email, password }) {
+  User.signup = async function ({ username, email, password }) {
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({
       username,
--- db/seeders/20200504043519-insert-users.js
+++ db/seeders/20200504043519-insert-users.js
@@ -15,9 +15,9 @@ function r(o) {
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
-      r({ username: 'Demo-lition', email: 'demo@example.com', hashedPassword: createPassword() }),
-      r({ username: 'Yusuke', email: 'yusuke@example.com', hashedPassword: createPassword() }),
-      r({ username: 'Peta', email: 'petra@example.com', hashedPassword: createPassword() }),
+      r({ username: 'Demo-lition', email: 'demo@example.com', hashedPassword: createPassword(), imageUrl: '', displayBackgroundUrl: '', status: 'away', pronouns: 'she/her/hers', roles: '', departmentOrTeam: '', aboutMe: '' }),
+      r({ username: 'Yusuke', email: 'yusuke@example.com', hashedPassword: createPassword(), imageUrl: '', displayBackgroundUrl: '', status: 'appearOffline', pronouns: 'he/him/his', roles: '', departmentOrTeam: '', aboutMe: '' }),
+      r({ username: 'Peta', email: 'petra@example.com', hashedPassword: createPassword(), imageUrl: '', displayBackgroundUrl: '', status: 'busy', pronouns: 'they/them/theirs', roles: '', departmentOrTeam: '', aboutMe: '' }),
     ]);
   },
 
